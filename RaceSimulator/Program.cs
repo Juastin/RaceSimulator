2fb0cfbe5f4c200702940ce5be062b76a1f35684
Justin Passchier <juspasschier@gmail.com>
2021-09-15 17:12:02 +0200

    changed console output

--- RaceSimulator/Program.cs
+++ RaceSimulator/Program.cs
@@ -6,7 +6,7 @@ namespace RaceSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("RaceSimulator");
         }
     }
 }
