17a003e6ef861c92a16b016fb52321276673a051
Justin Passchier <juspasschier@gmail.com>
2021-09-17 15:06:58 +0200

    Added classes and implemented interface

---
+++ Model/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Driver : IParticipant
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Points { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IEquipment Equipment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public TeamColors TeamColors { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Driver()
+        {
+
+        }
+    }
+}
---
+++ Model/IParticipant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public enum TeamColors
+{
+    Red,
+    Green,
+    Yellow,
+    Grey,
+    Blue
+}
+namespace Model
+{
+    interface IParticipant
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public IEquipment Equipment { get; set; }
+        public TeamColors TeamColors { get; set; }
+
+    }
+}
